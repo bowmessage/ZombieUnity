23601b07a66506d874e00cc605f061093446b56a
AggieDev <waylonjbrown@gmail.com>
2013-08-26 21:53:09 -0500

    first fix commit

--- Assets/My Assets/Scripts/SmoothFollow2D.js
+++ Assets/My Assets/Scripts/SmoothFollow2D.js
@@ -4,11 +4,13 @@ var target : Transform;
 var smoothTime = 0.25;
 private var thisTransform : Transform;
 private var velocity : Vector3;
+private var correction : Vector3;
 
 function Start()
 {
 	thisTransform = transform;
 	target = GameObject.FindWithTag ("Player").transform;
+	correction = new Vector3(1,0,0);
 }
 
 function Update() 
@@ -19,5 +21,6 @@ function Update()
 			target.position.x, velocity.x, smoothTime);
 		thisTransform.position.z = Mathf.SmoothDamp( thisTransform.position.z, 
 			target.position.z, velocity.z, smoothTime);
+		//thisTransform.position += correction;
 	}
 }
