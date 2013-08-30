7a6380c36cc7c6fa01af29a8a1eec02616bfa237
James Bowman <bowmessage@gmail.com>
2013-08-29 20:44:48 -0500

    Changes starting navmesh.

---
+++ Assets/My Assets/NavmeshFollower.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavmeshFollower : MonoBehaviour {
+	
+	private GameObject navmeshObj;
+	private Mesh navmesh;
+	
+	
+	// Use this for initialization
+	void Start () {
+		navmeshObj = GameObject.FindGameObjectWithTag("Navmesh");
+		if(navmeshObj == null){
+			print ("Error: No navmesh found.");
+		}
+		MeshFilter navmeshObjMeshFilter = (MeshFilter)navmeshObj.GetComponent<MeshFilter>();
+		if(navmeshObjMeshFilter == null){
+			print ("Error: Navmesh object has no mesh filter.");
+		}
+		
+		
+		//This is the actual mesh which stores vertex and face information.
+		navmesh = navmeshObjMeshFilter.mesh;
+		
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
