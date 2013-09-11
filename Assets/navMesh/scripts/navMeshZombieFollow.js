b49124863eb3e410c1bf5365602c0e447d0f6db9
James Bowman <bowmessage@gmail.com>
2013-09-10 21:11:09 -0500

    Commit b4 i update the gitignore

---
+++ Assets/navMesh/scripts/navMeshZombieFollow.js
@@ -0,0 +1,53 @@
+﻿#pragma strict
+import System.Collections.Generic;
+
+ 
+
+var navMeshScript : navMesh;
+var path : List.<Vector3> = null;
+var targetNodeIndex : int = 0;
+var targetNode : Vector3;
+var playerToFollow : GameObject;
+ 
+
+var moveSpeed : float = 3.0;
+var distanceToNode : float = 3.0;
+
+function Start () {
+    //Find the navmesh script in the scene
+    navMeshScript = GameObject.FindObjectOfType(navMesh).GetComponent("navMesh");
+    playerToFollow = GameObject.FindGameObjectWithTag("Player");
+}
+
+ 
+
+function Update () {
+    //If the path is null
+    if (path == null) {
+        //Find a new random path
+        path = navMeshScript.findPath(transform.position, playerToFollow.transform.position);
+        targetNodeIndex = 0;
+    }
+
+    
+
+    //The end of the path hasn't been reached
+
+    if (targetNodeIndex < path.Count) {
+        targetNode = path[targetNodeIndex];
+        //If the character is close to the target node
+        if (Vector3.Distance(transform.position, targetNode) < distanceToNode) {
+            //Target the next node
+            targetNodeIndex += 1;
+        }
+    }
+
+    //The end of the path has been reached
+    else {
+        path = null;
+    }
+
+    //Move towards target node
+    transform.position += moveSpeed*(targetNode-transform.position).normalized*Time.deltaTime;
+
+}
